e1cf0bbc31d6cb93b6410f9748d855bd100441ab
Runmin Zhang <z.runmin@gmail.com>
2018-04-01 17:43:58 -0500

    SLM test. Integrate all the code. Make a test builder.

--- Backtesting/Vectorized/cross_compare.py
+++ Backtesting/Vectorized/cross_compare.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from .backtest import vectorizedbacktest
+
+from Utils.IOUtils import *
+from Utils.lm import *
+from Backtesting.Vectorized.Strategy import MovingAverageStrategy, SLMStrategy
+from Backtesting.Vectorized.backtest import vectorizedbacktest
 
 class ensembler:
     def __init__(self, tester, signals, tcas=None, labels=None):
@@ -21,7 +25,7 @@ class ensembler:
             self.labels = labels
         except:
             self.labels = list(range(1, len(signals)+1))
-            
+
         self.ensembles = None
         self.results = None
 
@@ -61,6 +65,7 @@ class ensembler:
             self.performance.index.name = 'Model Order'
         except:
             print('Failed to build the performance DataFrame')
+        return self.performance
 
     def plot(self, target_col="equitycurve", ax=None):
         """
@@ -85,3 +90,44 @@ class ensembler:
         ax.set_title(target_col)
 
         return ax
+
+
+def compile_signal(data, slm, max_order=8):
+    slm = slm[['prior', 'max']]
+    slm = slm.rename(columns={'max': 'signal'})
+    signals = [SLMStrategy(data, slm, m).generatingsignal() for m in np.arange(1, max_order+1)]
+    return signals
+
+
+class tester:
+    def __init__(self, DATA_DIR, OUTPUT_DIR):
+        self._DATA_DIR = DATA_DIR
+        self._OUTPUT_DIR = OUTPUT_DIR
+
+    def build(self, commodity, exp_date, model_order=7, freq='5min', offset=0, start='20160701', end='20161031', tca=None):
+        self.config = {
+            'commodity': commodity,
+            'exp_date': exp_date,
+            'model_order': model_order,
+            'freq:': freq,
+            'offset': offset,
+            'start_day': start,
+            'end_day': end,
+            'tca': tca
+        }
+
+        
+        self.slm = LM_model(data_root_dir=str(self._DATA_DIR),n=model_order)
+        self.prob_table = self.slm.LM(commodity=commodity, exp_list=[exp_date], offset=offset, flg='train') # TODO: Fix the training period
+
+        self.test_data = vectorizedbacktest.compile_data(self._DATA_DIR, commodity, exp_date,   start = start, end=end)
+        signals = compile_signal(self.test_data, self.prob_table, max_order=model_order)
+        self.ensemble = ensembler(vectorizedbacktest, signals, tcas=tca)
+        self.ensemble.build()
+
+    def run(self):
+        self.results = self.ensemble.run()
+        self.performance = self.ensemble.calperformance()
+
+    def plot(self, target_col="equitycurve", ax=None):
+        return self.ensemble.plot(target_col, ax)
--- Backtesting/Vectorized/main.py
+++ Backtesting/Vectorized/main.py
@@ -3,50 +3,34 @@ import sys
 sys.path.append('/home/rz14/Documents/QR_Qishi/QishiQR/')
 
 from pathlib import Path
-import matplotlib.pyplot as plt
-import numpy as np
 
-from Utils.IOUtils import *
-from Utils.lm import *
-from Backtesting.Vectorized.Strategy import MovingAverageStrategy, SLMStrategy
-from Backtesting.Vectorized.backtest import vectorizedbacktest
-from Backtesting.Vectorized.cross_compare import ensembler
+from Backtesting.Vectorized.cross_compare import *
+
+import warnings
+warnings.filterwarnings(action='ignore')
 
 # ====== Initial Configuration ========
-ROOT_DIR = "/home/rz14/Documents/QR_Qishi/tick2016/"
-DATA_DIR = "/home/rz14/Documents/QR_Qishi/QishiQR/"
-OUTPUT = Path(DATA_DIR, "ag")
-# =====================================
 
-# Build DATA_PATH with Python Pathlib -- More organized than simple strings with OS library
-# https://www.scivision.co/python-idiomatic-pathlib-use/
-
-DATA_PATH = Path(ROOT_DIR, DATA_DIR)
-ASSET_PATH = Path(ROOT_DIR,OUTPUT_DIR, ASSET)
+ROOT_DIR = "/home/rz14/Documents/QR_Qishi/QishiQR/"
+DATA_DIR = "/home/rz14/Documents/QR_Qishi/tick2016/"
+OUTPUT_DIR = "/home/rz14/Documents/QR_Qishi/QishiQR/Output/"
+ASSET = 'ag'
+ASSET_PATH = Path(OUTPUT_DIR, ASSET)
 ASSET_PATH.mkdir(exist_ok=True, parents=True)
 
+model_order = 7
+freq = '5min'
+offset = 0
+tca = None
 
-## Scratch below ##
+start = '20160701'
+end = '20161031'
+commodity = ASSET
+exp_date = '1701'
 
-# ========= Build SLM table ==========
-DATA_ROOT = Path('/home/rz14/Documents/QR_Qishi/tick2016')
-OUTPUT_DIR = Path('/home/rz14/Documents/')
-slm = LM_model(data_root_dir=str(DATA_ROOT))
+# === Main Routine ===
 
-
-
-
-
-
-def compile_signal(DATA_PATH, max_order=6):
-    data = pd.read_csv(DATA_PATH / 'ag_1712_5min.csv')
-    slm = pd.read_csv(DATA_PATH / 'ag_5min_freq.csv')[['prior', 'max']]
-    slm = slm.rename(columns={'max': 'signal'})
-    signals = [SLMStrategy(data, slm, m).generatingsignal() for m in np.arange(1, max_order+1)]
-    return signals
-
-signals = compile_signal(DATA_PATH, max_order=6)
-
-tester_ensemble = ensembler(vectorizedbacktest, signals)
-tester_ensemble.build()
-tester_ensemble.run()
+test1 = tester(DATA_DIR, OUTPUT_DIR)
+test1.build(commodity, exp_date, model_order, freq, offset, start, end, tca)
+test1.run()
+test1.plot()
